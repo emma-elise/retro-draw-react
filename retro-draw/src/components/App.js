71a3792f826bd29d30394879575147fffe8deb8b
emma-elise <loirratse@gmail.com>
2021-08-08 08:04:43 -0400

    stretch goal - useEffect for localStorage

--- retro-draw/src/components/App.js
+++ retro-draw/src/components/App.js
@@ -1,13 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import Palette from "./Palette";
 import Grid from "./Grid";
 import ActionPanel from "./ActionPanel";
 import { COLORS, buildCellList } from "../utils";
 
+const getCellListFromLocal = () => {
+  let cellList = JSON.parse(localStorage.getItem("cellList"));
+  if (cellList) return cellList;
+  return buildCellList();
+};
+
+const setCellListOnLocal = (cellList) =>
+  localStorage.setItem("cellList", JSON.stringify(cellList));
+
 const App = () => {
   const [activeColor, setActiveColor] = useState(COLORS[0]);
-  const [cellList, setCellList] = useState(buildCellList);
+  const [cellList, _setCellList] = useState([]);
+
+  const setCellList = (newCellList) => {
+    setCellListOnLocal(newCellList);
+    _setCellList(newCellList);
+  };
+
+  useEffect(() => {
+    setCellList(getCellListFromLocal());
+  }, []);
+
   return (
     <div className="app">
       <Header />
